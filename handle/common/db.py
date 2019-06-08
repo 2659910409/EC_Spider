7ec5dbaeb3ade101537377e717cf35fbafdaddf8
shuai.liu <shuai.liu@shuyun.com>
2019-06-08 21:42:54 +0800

    2019/06/08 21:42 刘帅

--- handle/common/db.py
+++ handle/common/db.py
@@ -1,7 +1,20 @@
+import pymssql
+
 
 class DB:
+    def __init__(self, host, user, passwd, db_name, port=3306, charset='utf-8'):
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+        self.port = port
+        self.db_name = db_name
+        self.charset = charset
+
     def create_conn(self):
-        pass
+        db_conn = pymssql.connect(server=self.host, port=self.port, user=self.user, password=self.passwd,
+                                  database=self.db_name, charset=self.charset)
+        db_cur = db_conn.cursor()
+        return db_conn, db_cur
 
     def input(self, data):
         """
